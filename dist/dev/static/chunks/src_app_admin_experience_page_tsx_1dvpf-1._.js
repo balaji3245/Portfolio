7190a88b600beec4b1933503fbcb8ba09fc7561f
Balaji <chaughulebalaji09@gmail.com>
2026-07-12 22:39:23 +0530

    chore: ignore and remove dist directory from git

--- dist/dev/static/chunks/src_app_admin_experience_page_tsx_1dvpf-1._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_1t8d7y3._.js","static/chunks/src_app_admin_experience_page_tsx_1vpm70t._.js"],
-    source: "dynamic"
-});
--- dist/dev/static/chunks/src_app_admin_page_tsx_1dvpf-1._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_067c_vn._.js","static/chunks/src_app_admin_page_tsx_1po4qpu._.js"],
-    source: "dynamic"
-});
